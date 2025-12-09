92d83654a9b75f6c268d66ac278a4c0de0ba3a2e
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-12-09 19:54:18 +0100

    feat: fatto il bonus della grafica

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 export default function Home() {
     return (
-        <div className="text-center">
+            <div className=" miao">
+        <div className="text-center text-warning bg-secondary-subtle">
             <h1>Miaoazon</h1>
             <h3>benvenuti su miaoazon dove i vostri desideri divengono realtà</h3>
-
+        </div>
         </div>
     )
 }
